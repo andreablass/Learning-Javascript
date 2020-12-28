db7fffec52f58d166d1061a942d28e671ef78937
andrea <andreablass11@gmail.com>
2020-12-28 12:57:24 -0600

    Second commit

---
+++ variables.js
@@ -0,0 +1,19 @@
+//var, variables globales
+var saludo ='HOla andrea'
+if(saludo){
+    var saludo = 'Hola'
+    console.log(saludo)
+}
+console.log(saludo)
+//let, variables limitadas al alcance del bloque donde las definimos
+let nombre = 'LOL'
+if(nombre){
+    let nombre = 'Andy'
+    console.log(nombre)
+}
+console.log(nombre)
+
+//const, variables inmutables
+const PI = 3.14
+console.log(PI)
+
