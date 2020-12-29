eafe06a926c2bf56b61f14f8c7408f531bbf93bd
andrea <andreablass11@gmail.com>
2020-12-29 11:41:36 -0600

    Funciones

---
+++ funciones.js
@@ -0,0 +1,17 @@
+//SOn un conjunto de sentecnias que realizan una tarea
+
+function saludo(){
+    console.log('Hello Andrea desde la función')
+}
+
+var miSuma = function(a, b, c) {
+    return a +b + c 
+}
+saludo()
+console.log(miSuma(4,5, 1))
+var a2 = 4
+var b2 = 9
+var c2 = 5
+console.log(miSuma(4,6, 7))
+
+console.log(miSuma(a2,b2, c2))
